8a8d118e3df035758d5199170345a0fba090c083
Prashant Singh <prashant@prashtx.com>
2012-09-15 23:42:25 -0700

    Fix bug in fetching/processing GitHub data
    Explicitly specify the data type in the ajax request, since it wasn't
    being processed as JSON on Firefox.
    Sort by pushed_at, rather than updated_at.
    Straighten up some other pieces of code.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
-/*jslint browser: true global document: true */
+/*jslint browser: true */
+/*global document, ko, hashtub, $ */
 
 // Main ViewModel for the page.
 var PageVM = function() {
+  'use strict';
   var self = {};
 
   // Hashtags
@@ -17,8 +19,8 @@ var PageVM = function() {
       var tagTable;
       var tags = [];
       var repos = result.sort(function compare(a, b) {
-        var x = a.updated_at;
-        var y = b.updated_at;
+        var x = a.pushed_at;
+        var y = b.pushed_at;
         if (x < y) {
           return 1;
         } else if (x > y) {
@@ -28,33 +30,38 @@ var PageVM = function() {
       });
 
       // Repos
+      function getRepoInfo(repoVM) {
+        var repo = repoVM();
+        $.ajax({
+          url: repo.url,
+          dataType: 'json'
+        })
+          .done(function(data) {
+            // Add parent repo information
+            repo.fork_parent = {
+              name: data.parent.name,
+              user: data.parent.owner.login,
+              html_url: data.parent.html_url
+            };
+            repoVM(repo);
+          });
+      }
       for (i = 0; i < repos.length; i++) {
         repo = repos[i];
         repo.fork_parent = null;
-        if (repo.fork == true) {
+        if (repo.fork === true) {
           // Use an observable, so we can update as the data arrives.
           repo = ko.observable(repo);
           repos[i] = repo;
           // Get repo information
-          (function getRepoInfo(repoVM) {
-            var repo = repoVM();
-            $.ajax({url: repo.url})
-              .done(function(data) {
-                // Add parent repo information
-                repo.fork_parent = {
-                  name: data.parent.name,
-                  user: data.parent.owner.login,
-                  html_url: data.parent.html_url
-                };
-                repoVM(repo);
-              });
-          })(repo);
+          getRepoInfo(repo);
         }
       }
       self.repos(repos);
 
       // Hashtags
       tagTable = hashtub.makeTagTable(repos);
+      var tag;
       for (tag in tagTable) {
         if (tagTable.hasOwnProperty(tag)) {
           tags.push({ name: tag, repos: tagTable[tag] });
